3666da667ffa9b649b1320b849851dac0a8fc38f
Nadufa <nadufa18404@gmail.com>
2025-09-04 02:49:25 +0300

    refactor: webpack confog]

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,6 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
         exclude: /node_modules/,
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader'],
-      //   exclude: /node_modules/,
-      // },
     ],
   },
   plugins: [
